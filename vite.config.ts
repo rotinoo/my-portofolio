8cc0fd5c131a4af5bc594d780ce8c47030ad20e7
Rotinoo <robby.agn@gmail.com>
2024-10-06 12:54:20 +0700

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofoio/', // Change this to your GitHub repository name
+  base: "/my-portofolio/", // YOUR REPO NAME HERE
 });
